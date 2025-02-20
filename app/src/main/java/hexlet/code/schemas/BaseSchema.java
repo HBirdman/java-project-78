a23e7a35b6fdcef89cf2a8a375820029972ab0d6
HBirdman <hbirdman@mail.ru>
2025-02-20 18:35:42 +0300

    Removed redundant variable testCondition, added field isRequired for better readability

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -7,23 +7,26 @@ import java.util.function.Predicate;
 
 public class BaseSchema<T> {
     protected Map<String, Predicate<T>> schemas = new HashMap<>();
+    protected boolean isRequired = false;
 
+    void setIsRequired() {
+        isRequired = true;
+    }
     /**
      * This method checks whether the provided data matches the selected data conditions.
      * @param data - can be String, Integer, int or Map type
      * @return boolean
      */
     public boolean isValid(T data) {
-        if (schemas.containsKey("required") && (data == null || data.equals(""))) {
+        if (isRequired && (data == null || data.equals(""))) {
             return false;
         }
-        if (!schemas.containsKey("required") && (data == null || data.equals(""))) {
+        if (!isRequired && (data == null || data.equals(""))) {
             return true;
         }
         Set<Map.Entry<String, Predicate<T>>> schemasSet = schemas.entrySet();
         for (Map.Entry<String, Predicate<T>> schema : schemasSet) {
-            boolean testCondition = schema.getValue().test(data);
-            if (testCondition) {
+            if (schema.getValue().test(data)) {
                 return false;
             }
         }
@@ -35,6 +38,7 @@ public class BaseSchema<T> {
      * @return BaseSchema<T>
      */
     public BaseSchema<T> required() {
+        setIsRequired();
         schemas.put("required", (d) -> d == null || d.equals(""));
         return this;
     }
